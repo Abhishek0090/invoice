0fc39d38f84d4a7fb89b7c59c7897a186c309ff4
Abhishek0090 <palabhishek411@gmail.com>
2023-01-31 12:59:02 +0530

    added new fields

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View , StatusBar} from 'react-native';
 import CreateBill from './Screens/CreateBill';
 import HomeScreens from './Screens/HomeScreens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreens/>
-      {/* <Image source={require('./assets/icon.png')} style={{width:100,height:100}}/>
-      {/* <Button title='Abhishek' onPress={()=>{alert("hi bro")}}/> */}
-        {/* <TouchableOpacity   onPress={()=>{alert("hi bro")}} style={styles.button} >
-          <Text>Create Bill</Text>
-        </TouchableOpacity> */}   
+      <HomeScreens />
+      {/* <Image source={require('./assets/icon.png')} style={{ width: 100, height: 100 }} />
+      <Button title='Abhishek' onPress={() => { alert("hi bro") }} />
+      <TouchableOpacity onPress={() => { alert("hi bro") }} style={styles.button} >
+        <Text>Create Bill</Text>
+      </TouchableOpacity> */}
     </View>
   );
 }
@@ -19,19 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center', 
+    backgroundColor: '#fff', 
+    marginTop: StatusBar.currentHeight
+    },
+  button: {
+    height: 40,
+    backgroundColor: 'lightblue',
+    width: 100,
+    alignItems: 'center',
     justifyContent: 'center',
-  },
-  button : {
-    height : 40,
-    backgroundColor : 'lightblue',
-    width : 100,
-    alignItems : 'center',
-    justifyContent : 'center',
-    borderRadius  : 10, 
-    elevation  : 10,
-    top  : 10
+    borderRadius: 10,
+    elevation: 10,
+    top: 10
 
   }
 });
--- Screens/CreateBill.js
+++ Screens/CreateBill.js
@@ -1,14 +1,58 @@
 import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateBill = () => {
+
+    const [data, setData] = useState([{
+        name: '',
+        address: '',
+        mobile: ''
+    }]);
+
+    // const [name, setName] = useState();
+    // const [address, setAddress] = useState();
+    // const [mobile, setMobile] = useState();
+
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        setData((prev) => {
+            return {
+                ...prev, [e.target.name]: e.target.value
+            }
+        })
+
+    }
+
     return (
         <View style={styles.container}>
             <Text>CreateBill</Text>
             <ScrollView>
                 <View style={styles.inputContainer} >
                     <Text>Name</Text>
-                    <TextInput placeholder='Enter Your Name' style={styles.textInput} />
+                    <TextInput placeholder='Enter Your Name' style={styles.textInput} value={data.name} onChange={handleChange} name="name" />
+                </View>
+                <View style={styles.inputContainer} >
+                    <Text>Address</Text>
+                    <TextInput placeholder='Address' style={styles.textInput} value={data.address} onChange={handleChange} name="address" />
+                </View>
+                <View style={styles.inputContainer} >
+                    <Text>Mobile No</Text>
+                    <TextInput placeholder='Mobile' style={styles.textInput} value={data.mobile} onChange={handleChange} name="mobile" keyboardType='number-pad' />
+                </View>
+                <View>
+                {data.map((data)=>{
+                    return (
+                        <>
+                    <Text>{data.name}</Text>
+                    <Text>{data.address}</Text>
+                    <Text>{data.mobile}</Text>
+                        </>
+
+                    )
+                })
+
+                }
                 </View>
             </ScrollView>
         </View>
@@ -19,14 +63,23 @@ const CreateBill = () => {
 export default CreateBill
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        backgroundColor : '#fff',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
     },
-    inputContainer : {
-        marginTop : 15,
-        marginLeft : 15,
-        marginRight : 15,
+    inputContainer: {
+        marginTop: 15,
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    textInput: {
+        marginTop: 4,
+        height: 40,
+        borderColor: "#000",
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 4,
+        marginBottom: 6
     }
 
 })
